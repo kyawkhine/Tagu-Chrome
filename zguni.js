caf8d289145f3b3ea9a6f7502170ec5457ac8c39
kyawkhine <kyawkhine1992@gmail.com>
2013-02-25 04:39:41 +0630

    change .textContent to innerHtml
    .textContent can't show html tag .that's why innerHTML to be used and zg2uni script from solveware 

--- zguni.js
+++ zguni.js
@@ -22,365 +22,200 @@ styleNode.textContent   = "@font-face { font-family: 'MON3 Anonta 1'; src: local
 document.head.appendChild (styleNode);
 
 
-
-//var myFunction= function(a)
-//{
-
-//a=zg2uni(a);
-//alert(a);
-//} 
-
-function string2hex(i)
+// JavaScript Document
+function Z1_Uni(input)
 {
-    switch (i){
-            case 10 :
-                tmp="A";
-                break;
-            case 11 :
-                tmp="B";
-                break;
-            case 12:
-                tmp="C";
-                break;
-            case 13:
-                tmp="D";
-                break;
-            case 14:
-                tmp="E";
-                break;
-            case 15:
-                tmp="F";
-                break;
-            default:
-                tmp=""+i;//make string
-                break;
-        }
-    return tmp;
-}
-
-function zg2uni (input)
-{
-    var output = input;
-    
-    var zawgyi=new Array();
-    var unicode=new Array();
-    var inc=0;
-    var tmp;//for looping tmp value
-    var i;
-
-    inc++;
-    zawgyi[inc]=/(\u103D|\u1087)/g;
-    unicode[inc]="\u103E";
-
-    inc++;
-    zawgyi[inc]=/\u103C/g;
-    unicode[inc]="\u103D";
-
-    inc++;
-    zawgyi[inc]=/(\u103B|\u107E|\u107F|\u1080|\u1081|\u1082|\u1083|\u1084)/g;
-    unicode[inc]="\u103C";
-
-    inc++;
-    zawgyi[inc]=/(\u103A|\u107D)/g;
-    unicode[inc]="\u103B";
-
-    inc++;
-    zawgyi[inc]=/\u1039/g;
-    unicode[inc]="\u103A";
-
-    inc++;
-    zawgyi[inc]=/\u106A/g;
-    unicode[inc]="\u1009";
-    
-    inc++;
-    zawgyi[inc]=/\u106B/g;
-    unicode[inc]="\u100A";
-
-    inc++;
-    zawgyi[inc]=/\u106C/g;
-    unicode[inc]="\u1039\u100B";
-
-    inc++;
-    zawgyi[inc]=/\u106D/g;
-    unicode[inc]="\u1039\u100C";
-
-    inc++;
-    zawgyi[inc]=/\u106E/g;
-    unicode[inc]="\u100D\u1039\u100D";
-
-    inc++;
-    zawgyi[inc]=/\u106F/g;
-    unicode[inc]="\u100D\u1039\u100E";
-
-
-    inc++;
-    zawgyi[inc]= /\u1070/g;
-    unicode[inc]= "\u1039\u100F";
-
-    inc++;
-    zawgyi[inc]= /(\u1071|\u1072)/g;
-    unicode[inc]= "\u1039\u1010";
-
-
-     //start U+106x
-    //loop for pat sint 
-    for(i=0;i<=5;i++)
-    {
-       
-        
-        if(i!=4) {
-             inc++;
-            zawgyi[inc]= new RegExp(String.fromCharCode("0x106"+i),"g");
-            tmp=parseInt("106"+i, 16) - parseInt('60', 16);
-            unicode[inc]="\u1039"+String.fromCharCode("0x"+tmp.toString(16));
-        }
-       
-    }
-    
-    inc++;
-    zawgyi[inc]=/(\u1066|\u1067)/g;
-    unicode[inc]="\u1039\u1006";
-    
-    for(i=8;i<=9;i++)
-    {
-        inc++;
-        zawgyi[inc]= new RegExp(String.fromCharCode("0x106"+i),"g");
-        
-        tmp=parseInt("106"+i, 16) - parseInt('61', 16);
-        unicode[inc]="\u1039"+String.fromCharCode("0x"+tmp.toString(16));
-    }
-
-    inc++;
-    zawgyi[inc]= new RegExp("(\u1073|\u1074)","g");
-    unicode[inc]= "\u1039\u1011";
-    
-    for(i=5;i<=12;i++)
-    {
-        if(i!=11) {
-            inc++;
-            zawgyi[inc]= new RegExp(String.fromCharCode("0x107"+string2hex(i)),"g");
-
-            tmp=parseInt("107"+string2hex(i), 16) - parseInt('63', 16);
-            unicode[inc]="\u1039"+String.fromCharCode("0x"+tmp.toString(16));
-        }
-    }
-
-    inc++;
-    zawgyi[inc]=/\u1085/g;
-    unicode[inc]='\u1039\u101C';
-
-
-    inc++;
-    zawgyi[inc]=/\u1033/g;
-    unicode[inc]="\u102F";
-
-    inc++;
-    zawgyi[inc]=/\u1034/g;
-    unicode[inc]="\u1030";
-
-    inc++;
-    zawgyi[inc]=/\u103F/g;
-    unicode[inc]="\u1030";
-
-    inc++;
-    zawgyi[inc]=/\u1086/g;
-    unicode[inc]="\u103F";
-
-    inc++;
-    zawgyi[inc]=/\u1088/g;
-    unicode[inc]="\u103E\u102F";
-
-    inc++;
-    zawgyi[inc]=/\u1089/g;
-    unicode[inc]="\u103E\u1030";
-
-    inc++;
-    zawgyi[inc]=/\u108A/g;
-    unicode[inc]="\u103D\u103E";
-
-
-    //kinzi list
-
-    inc++;
-    zawgyi[inc]=/([\u1000-\u1021])\u1064/g;
-    unicode[inc]="\u1004\u103A\u1039$1";
-
-    
-
-    inc++;
-    zawgyi[inc]=/([\u1000-\u1021])\u108B/g;
-    unicode[inc]="\u1004\u103A\u1039$1\u102D";
-
-    inc++;
-    zawgyi[inc]=/([\u1000-\u1021])\u108C/g;
-    unicode[inc]="\u1004\u103A\u1039$1\u102E";
-
-    inc++;
-    zawgyi[inc]=/([\u1000-\u1021])\u108D/g;
-    unicode[inc]="\u1004\u103A\u1039$1\u1036";
-
-    inc++;
-    zawgyi[inc]=/\u108E/g;
-    unicode[inc]="\u102D\u1036";
-
-    inc++;
-    zawgyi[inc]=/\u108F/g;
-    unicode[inc]="\u1014";
-
-    inc++;
-    zawgyi[inc]=/\u1090/g;
-    unicode[inc]="\u101B";
-
-    inc++;
-    zawgyi[inc]=/\u1091/g;
-    unicode[inc]="\u100F\u1039\u1091";
-    
-
-    //fix ka bar
-    inc++;
-    zawgyi[inc]=/\u1019\u102C(\u107B|\u1093)/g;
-    unicode[inc]="\u1019\u1039\u1018\u102C";
-
-    inc++;
-    zawgyi[inc]=/(\u107B|\u1093)/g;
-    unicode[inc]="\u103A\u1018";
-
-    inc++;
-    zawgyi[inc]=/(\u1094|\u1095)/g;
-    unicode[inc]="\u1037";
-
-    //1096 á‚– zawgyi in unicod ??? I don't know
-    inc++;
-    zawgyi[inc]=/\u1096/g;
-    unicode[inc]="\u1039\u1010\u103D";
-
-
-    inc++;
-    zawgyi[inc]=/\u1097/g;
-    unicode[inc]="\u100B\u1039\u100B";
-
-    inc++;
-    zawgyi[inc]=/\u103C([\u1000-\u1021])([\u1000-\u1021])?/g;
-    unicode[inc]="$1\u103C$2";
-
-    inc++;
-    zawgyi[inc]=/([\u1000-\u1021])\u103C\u103A/g;
-    unicode[inc]="\u103C$1\u103A";
-
-    inc++;
-    zawgyi[inc]=/\u1031([\u1000-\u1021])(\u103E)?(\u103B)?/g;
-    unicode[inc]="$1$2$3\u1031";
-
-    inc++;
-    zawgyi[inc]=/([\u1000-\u1021])\u1031(\u103B|\u103C|\u103D)/g;
-    unicode[inc]="$1$2\u1031";
-
-    inc++;
-    zawgyi[inc]=/\u1032\u103D/g;
-    unicode[inc]="\u103D\u1032";
-
-    inc++;
-    zawgyi[inc]=/\u103D\u103B/g;
-    unicode[inc]="\u103B\u103D";
-
-    //reorder
-    inc++;
-    zawgyi[inc]=/\u103A\u1037/g;
-    unicode[inc]="\u1037\u103A";
-
-    inc++;
-    zawgyi[inc]=/(\u102F|\u1030)(\u102D|\u102E)/g;
-    unicode[inc]="$2$1";
-
-    inc++;
-    zawgyi[inc]=/(\u103E)(\u103B|\u1037)/g;
-    unicode[inc]="$2$1";
-
-    inc++;
-    zawgyi[inc]=/\u1025(\u103A|\u102C)/g;
-    unicode[inc]="\u1009$1";
-
-    inc++;
-    zawgyi[inc]=/\u1025\u102E/g;
-    unicode[inc]="\u1026";
-
-    inc++;
-    zawgyi[inc]=/\u1005\u103B/g;
-    unicode[inc]="\u1008";
-
-    inc++
-    zawgyi[inc]=/\u1036(\u102F|\u1030)/g;
-    unicode[inc]="$1\u1036";
-
-    inc++
-    zawgyi[inc]=/\u1031\u1037\u103E/g;
-    unicode[inc]="\u103E\u1031\u1037";
-
-    inc++
-    zawgyi[inc]=/\u1031\u103E\u102C/g;
-    unicode[inc]="\u103E\u1031\u102C";
-
-    inc++;
-    zawgyi[inc]=/\u105A/g;
-    unicode[inc]="\u102B\u103A";
-
-    inc++;
-    zawgyi[inc]=/\u1031\u103B\u103E/g;
-    unicode[inc]="\u103B\u103E\u1031";
-
-    inc++;
-    zawgyi[inc]=/(\u102D|\u102E)(\u103D|\u103E)/g;
-    unicode[inc]="$2$1";
-
-    inc++;
-    zawgyi[inc]=/\u102C\u1039([\u1000-\u1021])/g;
-    unicode[inc]="\u1039$1\u102C";
-
-
-    inc++;
-    zawgyi[inc]=/\u103C\u1004\u103A\u1039([\u1000-\u1021])/g;
-    unicode[inc]="\u1004\u103A\u1039$1\u103C";
-
-    inc++;
-    zawgyi[inc]=/\u1039\u103C\u103A\u1039([\u1000-\u1021])/g;
-    unicode[inc]="\u103A\u1039$1\u103C";
-
-    inc++;
-    zawgyi[inc]=/\u103C\u1039([\u1000-\u1021])/g;
-    unicode[inc]="\u1039$1\u103C";
-
-    inc++;
-    zawgyi[inc]=/\u1036\u1039([\u1000-\u1021])/g;
-    unicode[inc]="\u1039$1\u1036";
-
-    inc++;
-    zawgyi[inc]=/\u1092/g;
-    unicode[inc]="\u100B\u1039\u100C";
-
-    
-
-    inc++;
-    zawgyi[inc]=/\u104E/g; //áŽá€„á€ºá€¸
-    unicode[inc]="\u104E\u1004\u103A\u1038";
-
-    inc++;
-    zawgyi[inc]=/\u1040(\u102B|\u102C|\u1036)/g;
-    unicode[inc]="\u101D$1";
-
-    inc++;
-    zawgyi[inc]=/\u1025\u1039/g;
-    unicode[inc]="\u1009\u1039"
-
-    for(i=0;i<=inc;i++)
-    {
-        output=output.replace(zawgyi[i],unicode[i]);
-    }
-    return output;
-}
-
-
-
+  var output=input;
+
+   var tallAA = "\u102B";
+   var AA = "\u102C";
+   var vi = "\u102D";
+   // lone gyi tin
+   var ii = "\u102E";
+   var u = "\u102F";
+   var uu = "\u1030";
+   var ve = "\u1031";
+   var ai = "\u1032";
+   var ans = "\u1036";
+   var db = "\u1037";
+   var visarga = "\u1038";
+
+   var asat = "\u103A";
+
+   var ya = "\u103B";
+   var ra = "\u103C";
+   var wa = "\u103D";
+   var ha = "\u103E";
+   var zero = "\u1040";
    
+   
+   output =  output.replace( /\u106A/g, " \u1009");
+   output =  output.replace( /\u1025(?=[\u1039\u102C])/g, "\u1009"); //new
+    output =  output.replace( /\u1025\u102E/g, "\u1026"); //new
+   output =  output.replace( /\u106B/g, "\u100A");
+   output =  output.replace( /\u1090/g, "\u101B");
+   output =  output.replace( /\u1040/g, zero);
+
+   output =  output.replace( /\u108F/g, "\u1014");
+   output =  output.replace( /\u1012/g, "\u1012");
+   output =  output.replace( /\u1013/g, "\u1013");
+   /////////////
+
+
+   output =  output.replace( /[\u103D\u1087]/g, ha);
+   // ha
+   output =  output.replace( /\u103C/g, wa);
+   // wa
+   output =  output.replace( /[\u103B\u107E\u107F\u1080\u1081\u1082\u1083\u1084]/g, ra);
+   // ya yint(ra)
+   output =  output.replace( /[\u103A\u107D]/g, ya);
+   // ya
+
+   output =  output.replace( /\u103E\u103B/g, ya + ha);
+   // reorder
+
+   output =  output.replace( /\u108A/g, wa + ha);
+   output =  output.replace( /\u103E\u103D/g, wa + ha);
+   // wa ha
+
+   ////////////////////// Reordering
+
+   output =  output.replace( /(\u1031)?(\u103C)?([\u1000-\u1021])\u1064/g, "\u1064$1$2$3");
+   // reordering kinzi
+   output =  output.replace( /(\u1031)?(\u103C)?([\u1000-\u1021])\u108B/g, "\u1064$1$2$3\u102D");
+   // reordering kinzi lgt
+   output =  output.replace( /(\u1031)?(\u103C)?([\u1000-\u1021])\u108C/g, "\u1064$1$2$3\u102E");
+   // reordering kinzi lgtsk
+   output =  output.replace( /(\u1031)?(\u103C)?([\u1000-\u1021])\u108D/g, "\u1064$1$2$3\u1036");
+   // reordering kinzi ttt
+
+   ////////////////////////////////////////
+
+   output =  output.replace( /\u105A/g, tallAA + asat);
+   output =  output.replace( /\u108E/g, vi + ans);
+   // lgt ttt
+   output =  output.replace( /\u1033/g, u);
+   output =  output.replace( /\u1034/g, uu);
+   output =  output.replace( /\u1088/g, ha+u);
+   // ha  u
+   output =  output.replace( /\u1089/g, ha+uu);
+   // ha  uu
+
+   ///////////////////////////////////////
+
+   output =  output.replace( /\u1039/g, "\u103A");
+   output =  output.replace( /[\u1094\u1095]/g, db);
+   // aukmyint
+
+   ///////////////////////////////////////Pasint order human error
+    output =  output.replace( /([\u1000-\u1021])([\u102C\u102D\u102E\u1032\u1036]){1,2}([\u1060\u1061\u1062\u1063\u1065\u1066\u1067\u1068\u1069\u1070\u1071\u1072\u1073\u1074\u1075\u1076\u1077\u1078\u1079\u107A\u107B\u107C\u1085])/g, "$1$3$2");  //new
+   
+   
+   
+   /////////////
+
+   output =  output.replace( /\u1064/g, "\u1004\u103A\u1039");
+
+   output =  output.replace( /\u104E/g, "\u104E\u1004\u103A\u1038");
+
+   output =  output.replace( /\u1086/g, "\u103F");
+
+   output =  output.replace( /\u1060/g, '\u1039\u1000');
+   output =  output.replace( /\u1061/g, '\u1039\u1001');
+   output =  output.replace( /\u1062/g, '\u1039\u1002');
+   output =  output.replace( /\u1063/g, '\u1039\u1003');
+   output =  output.replace( /\u1065/g, '\u1039\u1005');
+   output =  output.replace( /[\u1066\u1067]/g, '\u1039\u1006');
+   output =  output.replace( /\u1068/g, '\u1039\u1007');
+   output =  output.replace( /\u1069/g, '\u1039\u1008');
+   output =  output.replace( /\u106C/g, '\u1039\u100B');
+   output =  output.replace( /\u1070/g, '\u1039\u100F');
+   output =  output.replace( /[\u1071\u1072]/g, '\u1039\u1010');
+   output =  output.replace( /[\u1073\u1074]/g, '\u1039\u1011');
+   output =  output.replace( /\u1075/g, '\u1039\u1012');
+   output =  output.replace( /\u1076/g, '\u1039\u1013');
+   output =  output.replace( /\u1077/g, '\u1039\u1014');
+   output =  output.replace( /\u1078/g, '\u1039\u1015');
+   output =  output.replace( /\u1079/g, '\u1039\u1016');
+   output =  output.replace( /\u107A/g, '\u1039\u1017');
+   output =  output.replace( /\u107B/g, '\u1039\u1018');
+   output =  output.replace( /\u107C/g, '\u1039\u1019');
+   output =  output.replace( /\u1085/g, '\u1039\u101C');
+   output =  output.replace( /\u106D/g, '\u1039\u100C');
+
+   output =  output.replace( /\u1091/g, '\u100F\u1039\u100D');
+   output =  output.replace( /\u1092/g, '\u100B\u1039\u100C');
+   output =  output.replace( /\u1097/g, '\u100B\u1039\u100B');
+   output =  output.replace( /\u106F/g, '\u100E\u1039\u100D');
+   output =  output.replace( /\u106E/g, '\u100D\u1039\u100D');
+
+   /////////////////////////////////////////////////////////
+
+   output =  output.replace( /(\u103C)([\u1000-\u1021])(\u1039[\u1000-\u1021])?/g, "$2$3$1");
+   // reordering ra
+   
+   //output =  output.replace( /(\u103E)?(\u103D)?([\u103B\u103C])/g, "$3$2$1");
+   // reordering ra
+   
+   output =  output.replace( /(\u103E)(\u103D)([\u103B\u103C])/g, "$3$2$1");
+    output =  output.replace( /(\u103E)([\u103B\u103C])/g, "$2$1");
+    
+    output =  output.replace( /(\u103D)([\u103B\u103C])/g, "$2$1");
+   
+  
+
+
+  
+   
+   
+   output = output.replace(/(([\u1000-\u101C\u101E-\u102A\u102C\u102E-\u103F\u104C-\u109F]))(\u1040)(?=\u0020)?/g, function($0, $1)
+   {
+      return $1 ? $1 + '\u101D' : $0 + $1;
+
+   }
+   );
+   // zero and wa
+   
+   
+    output = output.replace(/((\u101D))(\u1040)(?=\u0020)?/g, function($0, $1)
+   {
+      return $1 ? $1 + '\u101D' : $0 + $1;
+
+   }
+   );
+   // zero and wa
+   
+   
+ 
+
+
+   output = output.replace(/(([\u1000-\u101C\u101E-\u102A\u102C\u102E-\u103F\u104C-\u109F\u0020]))(\u1047)/g, function($0, $1)
+   {
+      return $1 ? $1 + '\u101B' : $0 + $1;
+
+   }
+   );
+   // seven and ra
+
+   output =  output.replace( /(\u1047)( ? = [\u1000 - \u101C\u101E - \u102A\u102C\u102E - \u103F\u104C - \u109F\u0020])/g, "\u101B");
+   // seven and ra
+   
+
+  /* output =  output.replace( /(\u1031)?([\u1000-\u1021])(\u1039[\u1000-\u1021])?([\u102D\u102E\u1032])?([\u1036\u1037\u1038]{0,2})([\u103B-\u103E]{0,3})([\u102F\u1030])?([\u102D\u102E\u1032])?/g, "$2$3$6$1$4$8$7$5");
+   // reordering storage order*/
+   
+   output =  output.replace( /(\u1031)?([\u1000-\u1021])(\u1039[\u1000-\u1021])?([\u102D\u102E\u1032])?([\u1036\u1037\u1038]{0,2})([\u103B-\u103E]{0,3})([\u102F\u1030])?([\u1036\u1037\u1038]{0,2})([\u102D\u102E\u1032])?/g, "$2$3$6$1$4$9$7$5$8");
+   // reordering storage order 
+   output = output.replace(ans+u, u+ans);
+   
+    output =  output.replace( /(\u103A)(\u1037)/g, "$2$1");
+   // For Latest Myanmar3
+
+
+   return output;
+
+
+}//Z1_Uni
 
 function tagPage()
 {  
@@ -388,7 +223,7 @@ function tagPage()
     
         var els = document.getElementsByClassName(facebook_wordbreaking_classes[i])
   
-	
+
         for (var j = 0; j < els.length; j++) {
 
 	 var thisNode = els[j];
@@ -411,11 +246,11 @@ function tagPage()
         
             if (regexUni.test(text) && !regexZG.test(text)) {
                 
-		
+
             } else {
 
-		text=zg2uni(text);
-		thisNode.textContent=text;
+		//text=zg2uni(text);
+		thisNode.innerHTML=Z1_Uni(thisNode.innerHTML);
 		thisNode.style.fontFamily = computedStyles.fontFamily + "," + useUnicodeFont;
             }
         }
@@ -432,10 +267,9 @@ function tagPage()
     
                  var prNode = thisNode.parentNode;
                  var text = thisNode.textContent;
-    		
                  if (!regexMM.test(text)) {
                      continue;
-			
+
                  }
 
                  var computedStyles = document.defaultView.getComputedStyle(prNode, null);//check for font declaration
@@ -452,7 +286,7 @@ function tagPage()
  		// nofontfamily so unicode or zawgyi check mal
 
                  var nextNode = thisNode;
-	                                                           
+
                     while (nextNode.nextSibling) {                                                         
                         nextNode = nextNode.nextSibling;
                         text += nextNode.textContent;
@@ -462,11 +296,11 @@ function tagPage()
                         if (regexUni.test(text) && !regexZG.test(text)) { //check whether unicode or zawgyi without font declaration
 
 				prNode.style.fontFamily = computedStyles.fontFamily + "," + useUnicodeFont; //if unicode
-			
-                        } else {
 
-				text=zg2uni(text);
-				prNode.textContent=text;
+                        } else {
+				prNode.innerHTML=Z1_Uni(prNode.innerHTML);
+				//text=zg2uni(text);
+				//prNode.textContent=text;
 				prNode.style.fontFamily = computedStyles.fontFamily + "," + useUnicodeFont;
 
                         }
